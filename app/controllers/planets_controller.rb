5e9840452c5cc38542f9d800ca92cf658626cc16
Michaeldean147 <michaeldean147@yahoo.com>
2015-02-27 18:37:24 -0800

    added endor pic to app

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -3,6 +3,7 @@ class PlanetsController < ApplicationController
   def index
     planets = Planet.all
     render json: planets
+    puts ActionController::Base.helpers.asset_path("Endor.jpg")
   end
 
 end
